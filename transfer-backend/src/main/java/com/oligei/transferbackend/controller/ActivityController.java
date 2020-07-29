4d0e2436583eae87962c8b473cd3219cb7bccefb
feax <48175749+feaaaaaa@users.noreply.github.com>
2020-07-30 07:51:45 +0800

    Update ActivityController.java

--- transfer-backend/src/main/java/com/oligei/transferbackend/controller/ActivityController.java
+++ transfer-backend/src/main/java/com/oligei/transferbackend/controller/ActivityController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Activity")
+@RequestMapping("/activity")
 @CrossOrigin(origins = "*",maxAge = 3600)
 public class ActivityController {
 
