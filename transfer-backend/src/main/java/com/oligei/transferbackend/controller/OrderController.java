f7f60e6aa253194fdd2e20817fd26e0ca4f991cc
feax <48175749+feaaaaaa@users.noreply.github.com>
2020-07-30 07:52:10 +0800

    Update OrderController.java

--- transfer-backend/src/main/java/com/oligei/transferbackend/controller/OrderController.java
+++ transfer-backend/src/main/java/com/oligei/transferbackend/controller/OrderController.java
@@ -12,7 +12,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Order")
+@RequestMapping("/order")
 @CrossOrigin(origins = "*",maxAge = 3600)
 public class OrderController {
     @Autowired
