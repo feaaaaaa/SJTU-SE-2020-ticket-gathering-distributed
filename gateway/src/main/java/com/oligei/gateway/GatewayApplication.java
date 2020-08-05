5f96bf994acb94e93dfd1f9354c4126f6a2ae3d5
ziliuziliu <851123942@qq.com>
2020-08-05 18:33:26 +0800

    gateway renaming

--- gateway/src/main/java/com/oligei/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/oligei/gateway/GatewayApplication.java
@@ -11,9 +11,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
-public class TransferBackendApplication {
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TransferBackendApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
