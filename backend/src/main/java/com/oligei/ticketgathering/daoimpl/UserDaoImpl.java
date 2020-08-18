5b0af02686dd5e570ad4a03fe213a38253475683
ziliuziliu <851123942@qq.com>
2020-08-18 13:23:39 +0800

    update .gitignore

--- backend/src/main/java/com/oligei/ticketgathering/daoimpl/UserDaoImpl.java
+++ backend/src/main/java/com/oligei/ticketgathering/daoimpl/UserDaoImpl.java
@@ -1,10 +1,3 @@
-/**
- * @ClassName User
- * @Description User Dao Implementation
- * @Author ziliuziliu
- * @Date 2020/7/10
- */
-
 package com.oligei.ticketgathering.daoimpl;
 
 import com.oligei.ticketgathering.dao.UserDao;
@@ -37,35 +30,29 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     /**
-     *@Description check username and password
-     *@Param [username, password]
-     *@return com.oligei.ticketgathering.entity.mysql.User
-     *@Author Yang Yicheng
+     *check username and password
+     *@param username,password
+     *@return the saved user or null
+     *@author ziliuziliu,Yang Yicheng
      *@date 2020/8/18
-     *@Throws NullPointerException user is null or the password is incorrrect
+     *@throws NullPointerException if username or password is null
      */
     public User login(String username, String password) {
-//        User user=new User(1,"oligei","Male","123456","123456", "123456","123456","123456");
+        Objects.requireNonNull(username,"null username --UserDaoimpl login");
+        Objects.requireNonNull(password,"null password --UserDaoimpl login");
         User user = userRepository.checkUser(username);
-        if (user != null && encoder.matches(password, user.getPassword())){
-//            Integer userId = user.getUserId();
-//            UserMongoDB user_mongodb = userMongoDBRepository.findByUserId(userId);
-//            if (user_mongodb != null && user_mongodb.getPersonIcon() != null)
-//                user.setPersonIcon(user_mongodb.getPersonIcon());
-            return user;
-        }
-        else{
-            throw new NullPointerException("user is null or the password is incorrrect");
-        }
+        if (user == null) return null;
+        if (encoder.matches(password, user.getPassword())) return user;
+        return null;
     }
 
     @Override
     /**
-     *@description decode password, save the user information in mongodb&mysql
-     *@param user  user to be registered
+     *decode password, save the user information in mongodb&mysql
+     *@param user
      *@return register success or fail
-     *@author
-     *@date
+     *@author ziliuziliu,feaaaaaa
+     *@date 2020/8/18
      *@throws NullPointerException if user is null or the personIcon or password or username of user is null
      *@throws InvalidDataAccessApiUsageException if invalid data(include null) used in repository
      */
@@ -87,33 +74,34 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     /**
-     *@Description check whether the user is exsisted
-     *@Param [username]
-     *@return boolean
-     *@Author Yang Yicheng
+     *check whether the user is existed
+     *@param username
+     *@return true if existed, false if not
+     *@author ziliuzilu,Yang Yicheng
      *@date 2020/8/18
+     *@throws NullPointerException if username is null
      */
     public boolean existsByUsername(String username) {
+        Objects.requireNonNull(username,"null username --UserDaoimpl existsByUsername");
         User user = userRepository.findUserByUsername(username);
         return user != null;
     }
 
     @Override
     /**
-     *@Description get userInfo by userId
-     *@Param [userId]
-     *@return com.oligei.ticketgathering.entity.mysql.User
-     *@Author Yang Yicheng
+     *get userInfo by userId
+     *@param userId
+     *@return the user of certain userId, or null if not exists
+     *@author ziliuziliu,Yang Yicheng
      *@date 2020/8/18
-     *@Throws NullPointerException if userId is not exist
+     *@throws NullPointerException if userId is null
      */
     public User findUserByUserId(Integer userId){
+        Objects.requireNonNull(userId,"null userId --UserDaoimpl findUserByUserId");
         User user=userRepository.findUserByUserId(userId);
         UserMongoDB userIcon=userMongoDBRepository.findByUserId(userId);
-        if(user==null ||userIcon==null){
-            throw new NullPointerException("invalid userId");
-        }
-        else{
+        if (user == null || userIcon == null) return null;
+        else {
             user.setPersonIcon(userIcon.getPersonIcon());
             return user;
         }
