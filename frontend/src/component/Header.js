7ea4a73d963c07f7346acc73bb1ccdcc2effe08e
feax <48175749+feaaaaaa@users.noreply.github.com>
2020-09-07 21:04:02 +0800

    fix

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -181,9 +181,9 @@ export class HeaderInfo extends React.Component {
                                             <Menu.Item>
                                                 <a href="/admin">添加活动</a>
                                             </Menu.Item>
-                                            <Menu.Item>
-                                                <a href= "/login">订单管理</a>
-                                            </Menu.Item>
+//                                             <Menu.Item>
+//                                                 <a href= "/login">订单管理</a>
+//                                             </Menu.Item>
                                         </Menu>
                                     )}>
                                     <Button> Admin </Button>
